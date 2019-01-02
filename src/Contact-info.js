254f13783ea2564d3add10b50d1c233fe8699974
Garrett Richards <garrettxaivier@gmail.com>
2019-01-02 14:41:41 -0800

    finishedn contact list

--- src/Contact-info.js
+++ src/Contact-info.js
@@ -33,15 +33,12 @@ class Contact extends Component {
     render() {
       return (
        <div id="container2">
-        <span><Link to="/"><i class="fa fa-arrow-left"></i></Link></span>
-            <div><img src={this.state.img}/></div>
-                 <ul>
-                  
-                    <li id="names">{this.state.first} {this.state.last}</li>
-                    <li id="email">{this.state.email}</li>
-                    <li id="phone">{this.state.phone}</li>
-                    <li id="location">{this.state.city}, {this.state.state}</li>
-              
+            <div id="photo1"><span><Link to="/"><i class="fa fa-arrow-left"></i></Link></span><img src={this.state.img} id="photo"/></div>
+                 <ul id="lists">
+                    <li id="names"><i class="fa fa-user"></i> {this.state.first} {this.state.last}</li>
+                    <li id="email"><i class="fa fa-envelope"></i> {this.state.email}</li>
+                    <li id="phone"><i class="fa fa-phone"></i> {this.state.phone}</li>
+                    <li id="location"><i class="fa fa-globe"></i> {this.state.city}, {this.state.state}</li>
                 </ul>
         </div>
       );
